f33155df26be5f2eedd85c412f38cce8b20748de
Andrew357965 <andkatsa@hotmail.com>
2026-05-24 20:38:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def get_stock_data(ticker, start_date, end_date):
 def eda(df):
     # Flatten MultiIndex columns
     df = df.reset_index()
-    df.rename(columns={'index': 'date'})
+    df.rename(columns={'index': 'date'}, inplace=True)
     df.columns = [col[0] if isinstance(col, tuple) else col for col in df.columns]
 
     df.columns = [col.lower() for col in df.columns]
